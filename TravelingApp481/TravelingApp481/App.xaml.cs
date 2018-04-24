b77f624d8d8d2c53b81c954597fbb8fff46a0b20
Juan Madrigal <Madrigal.jmm@gmail.com>
2018-04-23 18:24:13 -0700

    First Commit

---
+++ TravelingApp481/TravelingApp481/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using TravelingApp481.ViewModels;
+using TravelingApp481.Views;
+using Xamarin.Forms;
+
+namespace TravelingApp481
+{
+    public partial class App : PrismApplication
+    {
+        //public App ()
+        //{
+        //	InitializeComponent();
+
+        //	MainPage = new TravelingApp481.MainPage();
+        //}
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            Debug.WriteLine($"**** {this.GetType().Name}.{nameof(OnInitialized)}");
+            InitializeComponent();
+
+            NavigationService.NavigateAsync(nameof(MainPage));
+        }
+        protected override void RegisterTypes(Prism.Ioc.IContainerRegistry containerRegistry)
+        {
+            Debug.WriteLine($"**** {this.GetType().Name}.{nameof(RegisterTypes)}");
+
+            containerRegistry.RegisterForNavigation<MainPage, TravelingApp481PageViewModel>();
+        }
+            
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ TravelingApp481/TravelingApp481/ViewModels/TravelingApp481PageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Prism.Mvvm;
+using Xamarin.Forms;
+
+namespace TravelingApp481.ViewModels
+{
+	public class TravelingApp481PageViewModel : BindableBase
+	{
+		public TravelingApp481PageViewModel ()
+		{
+            Debug.WriteLine($"****{this.GetType().Name}:  ctor");
+			
+		}
+	}
+}
